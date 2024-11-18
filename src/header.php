b5d8b3df25ef089e293588f882766c37f33e4141
Markp33 <marrrk700@yahoo.com>
2024-11-18 11:39:25 +0100

    files

--- src/header.php
+++ src/header.php
@@ -3,9 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>header</title>
+    <title></title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    
-</body>
-</html>
+<body class="flex flex-col min-h-screen">
+<header class="bg-gray-400 text-white rounded-full mt-4 mx-8 sticky top-6">
+    <div class="container mx-auto flex justify-center items-center p-4">
+        <a href="index.php" class="absolute left-4 mr-6">
+            <img class="h-10" src="" alt="logo">
+        </a>
+        <nav>
+            <ul class="flex space-x-4">
+                <li><a href="index.php" class="hover:text-black ">Home</a></li>
+                <li><a href="spiegelspel.php" class="hover:text-black border-l border-white pl-4">Spiegelspelconcept</a></li>
+                <li><a href="dienst.php" class="hover:text-black border-l border-white pl-4">Dienstregeling</a></li>
+                <li><a href="contact.php" class="hover:text-black border-l border-white pl-4">Contact</a></li>
+            </ul>
+        </nav>
+        <a href="index.php" class="absolute right-4 mr-6">
+            0612345678
+        </a>
+    </div>
+</header>
+
---
+++ src/pages/index.php
@@ -0,0 +1,7 @@
+<?php include_once __DIR__ . '/../header.php'; ?>
+
+<main class="flex-grow p-4">
+<div class="w-full h-[60rem] bg-black ">
+</main>
+
+<?php include_once __DIR__ . '/../footer.php'; ?>
